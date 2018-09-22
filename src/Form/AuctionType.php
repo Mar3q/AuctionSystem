ae5a68d21938b363376da5a421e15216185efbee
Marek <notyzop@gmail.com>
2018-09-22 13:14:55 +0200

    add Form for creating new auctions

---
+++ src/Form/AuctionType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Auction;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AuctionType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title', TextType::class, ['label' => 'Tytuł'])
+            ->add('description', TextType::class, ['label' => 'Opis'])
+            ->add('price', NumberType::class, ['label'=> 'Cena'])
+            ->add('save', SubmitType::class, ['label' => 'Zapisz'])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => Auction::class,
+        ));
+    }
+}
